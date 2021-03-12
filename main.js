bd3bcb0803549c8bffd2e9ee446f097a77e9b20f
Lenin Lucas <63570798+leninjo@users.noreply.github.com>
2021-03-12 12:46:26 -0600

    Agregué el slide

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-$(document).ready(() => {
-  $("#lightgallery").lightGallery({
-    pager: true
-  });
-});
+var secondarySlider = new Splide( '#secondary-slider' ).mount();
+var primarySlider = new Splide( '#primary-slider' );
+primaryslider.sync( secondarySlider ).mount();
+
+var secondarySlider = new Splide( '#secondary-slider', {
+	rewind      : true,
+	fixedWidth  : 100,
+	fixedHeight : 64,
+	isNavigation: true,
+	gap         : 10,
+	focus       : 'center',
+	pagination  : false,
+	cover       : true,
+	breakpoints : {
+		'600': {
+			fixedWidth  : 66,
+			fixedHeight : 40,
+		}
+	}
+} ).mount();
+
+var primarySlider = new Splide( '#primary-slider', {
+	type       : 'fade',
+	heightRatio: 0.5,
+	pagination : false,
+	arrows     : false,
+	cover      : true,
+} );
+
+primarySlider.sync( secondarySlider ).mount();
